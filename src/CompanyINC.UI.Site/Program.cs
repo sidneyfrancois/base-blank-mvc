4f642d3fa324df784837ac4c1bf241732fe05f96
Sidney François <sidney.fvm.dev@outlook.com>
2023-07-06 09:58:58 -0300

    Add project files.

---
+++ src/CompanyINC.UI.Site/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionando MVC ao container
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Ativando página de erros caso seja ambiente de desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+
+// Adicionando rota padrão
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
---
+++ src/CompanyINC.UI.Site/Views/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CompanyINC.UI.Site.Views.Shared
+{
+    public class _LayouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
